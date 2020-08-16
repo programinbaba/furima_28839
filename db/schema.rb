cf56e8e657fb409eba44be8c33424a866f8bcc03
programinbaba <punk_hazard@icloud.com>
2020-08-16 09:03:29 +0900

    モデル・テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_15_024446) do
+ActiveRecord::Schema.define(version: 2020_08_15_233450) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_08_15_024446) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "sold_outs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_sold_outs_on_item_id"
+    t.index ["user_id"], name: "index_sold_outs_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,4 +50,6 @@ ActiveRecord::Schema.define(version: 2020_08_15_024446) do
   end
 
   add_foreign_key "items", "users"
+  add_foreign_key "sold_outs", "items"
+  add_foreign_key "sold_outs", "users"
 end
