47b009d60c483c4e682294e95294d74d472054ad
programinbaba <punk_hazard@icloud.com>
2020-08-19 18:10:58 +0900

    モデル・テーブル作成

---
+++ app/models/item.rb
@@ -0,0 +1,26 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  before_to_active_hash :category, :status, :cost, :prefecture, :day
+
+  # 空の投稿を保存できないようにする
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category
+    validates :status
+    validates :cost
+    validates :prefecture
+    validates :day
+    validates :price
+    validates :user
+  end
+
+  # ジャンルの選択が「---」の時は保存できないようにする
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :cost_id
+    validates :prefecture_id
+    validates :day_id
+  end
+end
