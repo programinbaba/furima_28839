7647978d3064778019844e93f00794d5df3d9ce9
programinbaba <punk_hazard@icloud.com>
2020-08-19 20:14:33 +0900

    引数に関するエラーの解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  before_to_active_hash :category, :status, :cost, :prefecture, :day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :cost
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
 
   belongs_to :user
   has_one_attached :image
