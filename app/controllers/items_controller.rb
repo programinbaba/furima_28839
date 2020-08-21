40ff61492571017904cc90cb704eebb0c474f8b5
programinbaba <punk_hazard@icloud.com>
2020-08-21 14:26:28 +0900

    商品の一覧表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,9 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: :index
 
   def index
+    # @items = Item.includes(:user).order("created_at DESC")
+    query = "SELECT * FROM items ORDER BY created_at DESC"
+    @items = Item.find_by_sql(query)
   end
 
   def new
