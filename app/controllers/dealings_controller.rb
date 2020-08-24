097a612631d4e2eb01db52cdc3059e5b91db2544
programinbaba <punk_hazard@icloud.com>
2020-08-24 09:10:55 +0900

    購入商品を画面に反映させる処理

--- app/controllers/dealings_controller.rb
+++ app/controllers/dealings_controller.rb
@@ -1,9 +1,16 @@
 class DealingsController < ApplicationController
+  before_action :move_to_login
   def index
-    
+    # カラムがitem_idなのは、ルーティングのネストで
+    @item = Item.find(params[:item_id])
   end
 
   def create
     
+  end
+
+  private
+  def move_to_login
+    redirect_to user_session_path unless user_signed_in?
   end
 end
