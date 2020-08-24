88d773200f6121548f8ae31492dbd23e75cc3c67
programinbaba <punk_hazard@icloud.com>
2020-08-24 10:46:59 +0900

    バリデーションの設定

--- app/models/dealing.rb
+++ app/models/dealing.rb
@@ -1,4 +1,12 @@
 class Dealing < ApplicationRecord
   belongs_to :item
   belongs_to :sold_out
+
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :house_num
+    validates :phone
+  end
 end
