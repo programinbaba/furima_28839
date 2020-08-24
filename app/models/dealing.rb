d94886812625ce0865f5fdec844eda3b2aa29e87
programinbaba <punk_hazard@icloud.com>
2020-08-24 17:02:53 +0900

    バリデーションの設定

--- app/models/dealing.rb
+++ app/models/dealing.rb
@@ -10,4 +10,14 @@ class Dealing < ApplicationRecord
     validates :house_num
     validates :phone
   end
+
+  # 郵便番号(「-」を含みかつ7桁)  \dは数字, {3}は3回
+  with_options format { with: /\A\d{3}[-]\d{4}\z/ } do
+    validates :postal_code
+  end
+
+  # 電話番号(数字が11桁)
+  with_options format {  with: /\A\d{11}\z/ } do
+    validates :phone
+  end
 end
