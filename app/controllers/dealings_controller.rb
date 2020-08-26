51a25c330f6386def16871aa3357c35c7ebb7169
programinbaba <punk_hazard@icloud.com>
2020-08-26 12:41:34 +0900

    同じメソッドの省略

--- app/controllers/dealings_controller.rb
+++ app/controllers/dealings_controller.rb
@@ -1,14 +1,13 @@
 class DealingsController < ApplicationController
   before_action :move_to_login
+  before_action :item_id_params
   before_action :move_to_toppage
+  before_action :move_to_top_page
   def index
-    # カラムがitem_idなのは、ルーティングのネストで
-    @item = Item.find(params[:item_id])
   end
 
   def create
     # 保存に失敗した時、@itemを定義していないとエラーが生じる
-    @item = Item.find(params[:item_id])
     # :token以外をデータベースに保存
     @dealing = UserDealing.new(dealing_params)
 
@@ -23,7 +22,6 @@ class DealingsController < ApplicationController
   end
 
   private
-
   def move_to_login
     redirect_to user_session_path unless user_signed_in?
   end
@@ -32,15 +30,18 @@ class DealingsController < ApplicationController
     params.permit(:postal_code, :prefecture_id, :city, :house_num, :building, :phone, :item_id).merge(user_id: current_user.id)
   end
 
+  def item_id_params
+    # カラムがitem_idなのは、ルーティングのネストで
+    @item = Item.find(params[:item_id])
+  end
+
   # 出品者はURLを直接入力して購入ページに遷移しようとすると、トップページに遷移
   def move_to_toppage
-    @item = Item.find(params[:item_id])
     redirect_to root_path if current_user.id == @item.user_id
   end
 
   # URLを直接入力し、購入済み商品の購入ページへ遷移しようとすると、トップページに遷移
   def move_to_top_page
-    @item = Item.find(params[:item_id])
     # もし商品のidと購入済みの商品のidが一致すればルートパスへ
     redirect_to root_path if @item.id == @item.sold_out.id
   end
