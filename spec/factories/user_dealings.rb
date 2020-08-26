f088b49d6a72d291dc64de392fa6961d13d3cd35
programinbaba <punk_hazard@icloud.com>
2020-08-26 10:47:26 +0900

    basic認証の導入

--- spec/factories/user_dealings.rb
+++ spec/factories/user_dealings.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user_dealing do
-    token { 'token_test' }
+    token         { 'token_test' }
     postal_code   { "111-1111" }
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
     city          { Gimei.address.city.kanji }
