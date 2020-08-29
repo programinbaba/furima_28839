bac9089aeec14590156f90944f401e87296efa6a
programinbaba <punk_hazard@icloud.com>
2020-08-29 16:23:18 +0900

    複数枚の画像投稿機能の仮実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -5,10 +5,27 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
     // 選択した画像を表示する関数
     const createImageHTML = (blob) => {
       const imageElement = document.createElement("div")
+      imageElement.setAttribute("id", "image-element")
+      let imageElementNum = document.querySelectorAll("#image-element").length
+
       const blobImage = document.createElement("img")
       blobImage.setAttribute("src", blob)
+
+      const inputHTML = document.createElement("input")
+      inputHTML.setAttribute("id", `item_image_${imageElementNum}`)
+      inputHTML.setAttribute("name", "message[images][]")
+      inputHTML.setAttribute("type", "file")
+
       imageElement.appendChild(blobImage)
+      imageElement.appendChild(inputHTML)
       imageList.appendChild(imageElement)
+
+      inputHTML.addEventListener("change", (e) => {
+        file = e.target.files[0]
+        blob = window.URL.createObjectURL(file)
+
+        createImageHTML(blob)
+      })
     }
 
     document.getElementById("item-image").addEventListener("change", (e) => {
